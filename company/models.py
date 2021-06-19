74d8e8d59de562c262dae7351037ca49e864439d
yogesh <ysinge.aan@gmail.com>
2021-06-19 11:04:25 +0530

    modified models

--- company/models.py
+++ company/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Company(models.Model):
-     name = models.CharField(max_length=200)
-     location = models.CharField(max_length=200)
-     phone = models.IntegerField(default=0)
+     name = models.CharField(max_length=100)
+     location = models.CharField(max_length=100)
+     mobile = models.IntegerField(default=0)
      created_at = models.DateTimeField('created_at')
      update_at = models.DateTimeField('update_at')
 
